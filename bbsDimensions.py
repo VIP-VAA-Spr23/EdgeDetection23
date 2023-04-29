2c346c582eb3b299d07db39ae80fe947672240fd
Krishna Bendishankara <82446254+krishreddy2021@users.noreply.github.com>
2023-04-29 12:56:46 -0400

    comments

--- bbsDimensions.py
+++ bbsDimensions.py
@@ -1,67 +1,59 @@
 import os
 import csv
-import pickle
+
 
 if __name__ == "__main__":
     outputPath = os.path.abspath('outputs_csv/outputs_csv')
-    bbs_csv = os.listdir(outputPath) #Enter file path for folder that contains csv files
+    bbs_csv = os.listdir(outputPath) #takes in directory of csv files for each image with each containing bounding box dimensions
     gnd_locs = os.listdir("GndLocs")#Enter file path for manually created folder of all the location images
-    #img_locs = os.listdir("C:/Users/krish/OneDrive/Desktop/VIP/ImgLocs") 
     
-    with open('name_Pairs.csv', 'r', newline='') as csvfile:
+    with open('name_Pairs.csv', 'r', newline='') as csvfile: #reads and retreives a list of filename to location tuples
     # Create a CSV reader object
         reader = csv.reader(csvfile)
         pairs = list(reader)
-        
+
+    #this segment filters out the name_Pairs.csv by only including pairs with locations that are in the gnd_locs folder and saving it to gnd_pairs list
     gnd_pairs = []
     for loc in gnd_locs:
         loc = loc.split(".jpg")[0]
         for pair in pairs:
             if loc in pair[1]:
                 gnd_pairs.append(pair)
-    with open('gnd_pairs.csv', 'w', newline='') as csvfile:
+
+    with open('gnd_pairs.csv', 'w', newline='') as csvfile: #writes the gnd_pairs list into gnd_pairs.csv
         writer = csv.writer(csvfile)
         writer.writerows(gnd_pairs)
 
     csv_bbs = []
-    for pair in gnd_pairs:
+    for pair in gnd_pairs: #loops through each pair in gnd_pairs
         name = pair[0]
         name = name.replace(".jpg", ".csv")
         idx = bbs_csv.index(name)
-        csv_bbs.append(bbs_csv[idx])
+        csv_bbs.append(bbs_csv[idx]) #appends csv file of bounding box dimensions for every image in gnd_pairs
 
     bbsFiles = []
     nameCSV = []
     for i in range(len(csv_bbs)):
         csv_path = os.path.join(outputPath, csv_bbs[i])
-        with open(csv_path, mode="r") as csv_file:
-            reader = csv.reader(csv_file, delimiter=',')
+        with open(csv_path, mode="r") as csv_file: #opens and reads csv file from gnd_pairs
+            reader = csv.reader(csv_file, delimiter=',') #seperates the commas in the dimension list
             csvRows = []
             names = []
-            for row in reader:
-                csvRows.append(row)
-                names.append(csv_bbs[i])
+            for row in reader: #loops through every bounding box dimension in list of dimensions
+                csvRows.append(row) #appends dimensions
+                names.append(csv_bbs[i]) #file name for which the bounding box is part of
             csvRows.pop(0)
             names.pop(0)
-        bbsFiles.append(csvRows)
-        nameCSV.append(names)
+        bbsFiles.append(csvRows) #appends all the dimension rows to bbsFiles for each image file
+        nameCSV.append(names) #appends list of image files
 
-    bbsFiles = [ele for ele in bbsFiles if ele != []]
-    #nameCSV = [nameCSV[i] for i in range(len(bbsFiles)) if bbsFiles[i] != []]
-    print(len(bbsFiles))
-    print(len(nameCSV))
-    bbsFiles = sum(bbsFiles, [])
-    nameCSV = sum(nameCSV, [])
-    print(len(bbsFiles))
-    print(len(nameCSV))
-    print(type(bbsFiles[1]))
-    #bbsDict = {nameCSV[i]: bbsFiles[i] for i in range(len(nameCSV))}
-    #print(bbsDict)
-    bbsTP = tuple(zip(nameCSV, bbsFiles))
+    bbsFiles = [ele for ele in bbsFiles if ele != []] #removes elements that are empty lists
+    
+    bbsFiles = sum(bbsFiles, []) #concatenates all the lists in bbsFiles into one list
+    nameCSV = sum(nameCSV, []) #concatenates all lists together
+    bbsTP = tuple(zip(nameCSV, bbsFiles)) #creates tuples of filenames to its bounding box dimensions
 
-    with open('bbsTP.csv', 'w', newline='') as csvfile:
+    with open('bbsTP.csv', 'w', newline='') as csvfile: #saves bbsTP to bbsTP.csv
         writer = csv.writer(csvfile)
         writer.writerows(bbsTP)
-    #os.mkdir("C:/Users/krish/OneDrive/Desktop/VIP/bbsDict.pk1")
-    #with open("C:/Users/krish/OneDrive/Desktop/VIP/bbsDict.pk1", 'wb') as file:
-        #pickle.dump(bbsDict, file)
+    
--- createLOCS.py
+++ createLOCS.py
@@ -4,26 +4,26 @@ import cv2
 import csv
 
 def main():
-    with open('labeled_file_info.json', 'r') as f: #insert json file folder from downloads
+    with open('labeled_file_info.json', 'r') as f: #replace 'labeled_file_info.json' with the file path for your json file
         data = json.load(f)
-    names = [d["filename"] for d in data]
-    locations = [d["latlon"] for d in data]
-    latr = [loc[0] for loc in locations]
+    names = [d["filename"] for d in data] #extract filename for each element in file
+    locations = [d["latlon"] for d in data] #extract coordinates for each element
+    latr = [loc[0] for loc in locations] #extract latitude for each coordinate
     longs = [longs[1] for longs in locations]
     longs = list(set(longs))
-    lats = list(set(latr))
-    coords = list(zip(lats, longs))
-    print(len(data))
-    name_Pairs = tuple(zip(names, latr))
-    with open('name_Pairs.csv', 'w', newline='') as csvfile:
+    lats = list(set(latr)) #list of all the unique location latitudes in the data
+    
+    
+    name_Pairs = tuple(zip(names, latr)) #pair file names to its corresponding latitude 
+    with open('name_Pairs.csv', 'w', newline='') as csvfile: #write to new csv file
     # Create a CSV writer object
         writer = csv.writer(csvfile)
-
     # Write the list of tuples to the file
         writer.writerows(name_Pairs)
-    print(len(name_Pairs))
+
+    #This segment creates a list of unique locations that contains a seperate list of file names for each unique location
     fileLists = []
-    for lat in lats:
+    for lat in lats: 
         fileNames = []
         for element in data:
             if lat in element["latlon"]:
@@ -36,20 +36,20 @@ def main():
     
     
     i = 0
-    for file in fileLists:
+    for file in fileLists: #loops through every location list
         latd = str(lats[i])
-        pathDir = os.path.join("C:/Users/krish/OneDrive/Desktop/VIP/ImgLocs", latd) #create a folder for path
-        '''
-        os.mkdir(pathDir)
+        pathDir = os.path.join("C:/Users/krish/OneDrive/Desktop/VIP/ImgLocs", latd) #create a folder for path for ImgLocs that contains a seperate folder for each location
+        
+        os.mkdir(pathDir) #creating the directory
         i+= 1
-        for imgName in file:
+        for imgName in file: #loops through every img name in location list
             idx = fullDir.index(imgName)
             newImg = fullDir[idx]
             imgP = os.path.join(imgDir, newImg)
             img = cv2.imread(imgP)
-            cv2.imwrite(os.path.join(pathDir, newImg), img)
+            cv2.imwrite(os.path.join(pathDir, newImg), img) #creates a folder of locations that has a seperate folder for each location with its corresponding image files.
 
-    '''
+    
 
 if __name__ == "__main__":
    main()
